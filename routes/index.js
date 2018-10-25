2f8874615e26c6c24fda6943569c43992ed62bd4
Jari Duyvejonck <jari.duyvejonck.y0602@student.hogent.be>
2018-10-25 13:07:40 +0200

    exercise api call

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ let mongoose = require("mongoose");
 let Sessionmap = mongoose.model('sessionmap');
 let Page = mongoose.model('page');
 let Session = mongoose.model('session');
+let Exercises = mongoose.model('exercise');
 
 router.get('/API/sessionmap/:sessionmap', function (req, res, next) {
     res.json(req.sessionmap);
@@ -22,7 +23,7 @@ router.param('sessionmap', function (req, res, next, id) {
             return next(new Error('not found ' + id));
         }
 
-        let sessionQuery = Session.find({sessionmap_id: id});
+        let sessionQuery = Session.find({sessionmap_id: id}).sort({position: 1});
 
         sessionQuery.exec(function(err, sessions) {
             if (err) {
@@ -54,7 +55,7 @@ router.get('/API/exercise/:exercise', function (req, res, next) {
 });
 
 router.param('exercise', function (req, res, next, id) {
-    let query = Page.find({exercise_id: id});
+    let query = Exercises.find({session_id: id});
     query.exec(function (err, exercise) {
         if (err) {
             return next(err);
