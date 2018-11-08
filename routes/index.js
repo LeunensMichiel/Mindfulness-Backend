a0f4552a0c9d9ab8ebe92791790c3bc9e913c099
Nicolaas Leenknegt <nicolaas.leenknegt.w1123@student.hogent.be>
2018-11-08 21:13:35 +0100

    Paginalijst api calls aangemaakt en geprobeerd hooks aan te maken voor exercise, page en paragrapoh in het geval dat een parent object word verwijderd

--- routes/index.js
+++ routes/index.js
@@ -45,7 +45,7 @@ router.post('/API/sessionmap', function (req, res, next) {
 });
 
 router.delete('/API/sessionmap/:sessionmap', function (req, res) {
-    Sessionmap.remove({_id: {$in: req.sessionmap.sessions}}, function (err) {
+    Sessionmap.remove({ _id: { $in: req.sessionmap.sessions } }, function (err) {
         if (err) return next(err);
         req.sessionmap.remove(function (err) {
             if (err) {
@@ -123,7 +123,7 @@ router.param('sessionmap', function (req, res, next, id) {
     })
 });
 
-router.put('/API/session/:session', function(req, res, next){
+router.put('/API/session/:session', function (req, res, next) {
     let session = req.session;
     session.title = req.body.title;
     session.position = req.body.position;
@@ -141,7 +141,7 @@ router.get('/API/sessions/:sessionmapid', function (req, res, next) {
 });
 
 router.param('sessionmapid', function (req, res, next, id) {
-    let query = Session.find({"sessionmap_id": id});
+    let query = Session.find({ "sessionmap_id": id });
     query.exec(function (err, sessions) {
         if (err) {
             return next(err);
@@ -205,8 +205,186 @@ router.get('/API/exercises/:exercise', function (req, res, next) {
     res.json(req.exercise);
 });
 
+router.get('/API/exerciseWpages/:excerciseWpages', function (req, res, next) {
+    res.json(req.exercise);
+});
+
+router.param('excerciseWpages', function (req, res, next, id) {
+    let query = Exercise.findOne({ _id: id })
+        .populate({
+            path: 'pages',
+            populate: {
+                path: 'paragraphs',
+                model: 'paragraph'
+            }
+        });
+    query.exec(function (err, ex) {
+        if (err) {
+            return next(err);
+        }
+        if (!ex) {
+            return next(new Error('not found ' + id));
+        }
+        req.exercise = ex;
+        return next();
+    })
+});
+
+router.post('/API/exerciseWpages', function (req, res, next) {
+    let ex = new Exercise({
+        title: req.body.title,
+        position: req.body.position,
+        session_id: req.body.session_id
+    });
+    ex.save(function (err, post) {
+        if (err) {
+            return next(err);
+        }
+        res.json(post);
+    });
+});
+
+router.post('/API/exercise/:excerciseWpages/page', function (req, res, next) {
+    Paragraph.insertMany(req.body.paragraphs, function (err, pars) {
+        if (err) { return next(err); }
+        req.body.paragraphs = [];
+        let page = new Page(req.body);
+        for (var i = 0; i < pars.length; i++) {
+            page.paragraphs.push(pars[i]);
+        }
+        page.exercise_id = req.exercise._id;
+        page.save(function (err, page) {
+            if (err) {
+                return next(err);
+            }
+            req.exercise.pages.push(page);
+            req.exercise.save(function (err, ex) {
+                if (err) { return next(err); }
+                res.json(page);
+            })
+        });
+    });
+});
+
+router.post('/API/page/:page/paragraph', function (req, res, next) {
+    let par = new Paragraph();
+    par.position = req.body.position;
+    par.type = req.body.type;
+    par.filename = req.body.filename;
+    par.pathname = req.body.pathname;
+    par.description = req.description;
+    par.save(function (err, par) {
+        if (err) { return next(err); }
+        req.page.paragraphs.push(par);
+        req.page.save(function (err, page) {
+            if (err) { return next(err); }
+            res.json(page);
+        });
+    })
+});
+
+router.delete('/API/page/:page', function (req, res, next) {
+    var parids = [];
+    for (var i = 0; i < req.page.paragraphs.length; i++) {
+        parids.push(req.page.paragraphs[i]._id);
+    }
+    Paragraph.deleteMany({ "_id": parids}, function (err, pars) {
+        if (err) { return next(err); }
+        Page.findByIdAndDelete(req.page._id, function (err, data) {
+            if (err) {return next(err); }
+            res.json(data);
+        });
+    });
+});
+
+router.param('page', function (req, res, next, id) {
+    let query = Page.findById(id, function (err, page) {
+        if (err) { return next(err); }
+        if (!page) { return next("Page not found id: " + id); }
+        req.page = page;
+        return next();
+    });  
+});
+
+router.delete('/API/exercise/:excerciseWpages', function (req, res) {
+    var pageids = [];
+    var parids = [];
+    for (var i = 0; i < req.exercise.pages.length; i++) {
+        pageids.push(req.exercise.pages[i]._id);
+        if (req.exercise.pages[i].type === "TEXT") {
+            for (var x = 0; x < req.exercise.pages[i].paragraphs.length; x++) {
+                parids.push(req.exercise.page[i].paragraphs[x]._id);
+            }
+        }
+    }
+    Paragraph.deleteMany({ "_id": parids }, function (err, pars) {
+        if (err) { return next(err); }
+        Page.deleteMany({ "_id": pageids }, function (err, pages) {
+            if (err) { return next(err); }
+            Exercise.deleteOne({ "_id": req.exercise._id }, function (err, ex) {
+                if (err) { return next(err); }
+                res.json(ex);
+            })
+        });
+    });
+})
+
+router.post('/API/exercise/:excerciseWpages', function (req, res, next) {
+    var pars = [];
+    for (var i = 0; i < req.body.pages.length; i++) {
+        if (req.body.pages[i].type == "TEXT") {
+            pars.push({
+                position: i,
+                paragraphs: req.body.pages[i].paragraphs
+            })
+        }
+        req.body.pages[i].paragraphs = [];
+    }
+    req.exercise.title = req.body.title;
+    req.exercise.position = req.body.position;
+    Page.deleteMany({ "exercise_id": req.exercise._id }, function (err, delPages) {
+        if (err) { return next(err); }
+        Page.insertMany(req.body.pages, function (err, pages) {
+            if (err) { return next(err); }
+            req.exercise.pages = [];
+            for (var i = 0; i < req.body.pages.length; i++) {
+                req.exercise.pages.push(pages[i]);
+            }
+            for (var i = 0; i < pars.length; i++) {
+                Paragraph.insertMany(pars[i].paragraphs, function (err, paragraphs) {
+                    for (var x = 0; x < paragraphs.length; x++) {
+                        req.exercise.pages[pars[x].position].paragraphs.push(paragraphs[x]);
+                        req.exercise.pages[pars[x].position].save(function (err, page) {
+                            if (err) { return next(err); }
+                        });
+                    }
+                    req.exercise.save(function (err, ex) {
+                        if (err) { return next(err); }
+                        res.json(ex);
+                    });
+                });
+            }
+
+        });
+    });
+});
+
+router.param('emptyExercise', function (req, res, next, id) {
+    let query = Exercise.findOne({ _id: id });
+    query.exec(function (err, ex) {
+        if (err) {
+            return next(err);
+        }
+        if (!ex) {
+            return next(new Error("Exercise not found id: " + id));
+        }
+        req.exercise = ex;
+        return next();
+    })
+})
+
 router.param('exercise', function (req, res, next, id) {
-    let query = Exercise.find({session_id: id});
+    let query = Exercise.find({ session_id: id });
     query.exec(function (err, exercise) {
         if (err) {
             return next(err);
@@ -224,7 +402,7 @@ router.get('/API/pages/:page', function (req, res, next) {
 });
 
 router.param('page', function (req, res, next, id) {
-    let query = Page.find({exercise_id: id}).populate('paragraphs');
+    let query = Page.find({ exercise_id: id }).populate('paragraphs');
 
     query.exec(function (err, pages) {
         if (err) {
@@ -244,7 +422,7 @@ router.get('/API/paragraphs/:paragraph', function (req, res, next) {
 });
 
 router.param('paragraph', function (req, res, next, id) {
-    let query = Paragraph.find({page_id: id});
+    let query = Paragraph.find({ page_id: id });
 
     query.exec(function (err, paragraph) {
         if (err) {
@@ -259,7 +437,7 @@ router.param('paragraph', function (req, res, next, id) {
 });
 
 //post
-    // werkt
+// werkt
 router.get('/API/posts', function (req, res, next) {
     let query = Post.find();
     query.exec(function (err, posts) {
@@ -282,7 +460,7 @@ router.get('/API/posts/:userid', function (req, res, next) {
 
 router.param('userid', function (req, res, next, id) {
     console.log("test");
-    let query = Post.find({"user_id": id});
+    let query = Post.find({ "user_id": id });
 
     query.exec(function (err, posts) {
         if (err) {
@@ -314,7 +492,7 @@ router.post('/API/post', function (req, res, next) {
     });
 }); */
 
-    // werkt
+// werkt
 router.post('/API/post', function (req, res, next) {
     let post = new Post({
         sessionmap_id: req.body.sessionmap_id,
