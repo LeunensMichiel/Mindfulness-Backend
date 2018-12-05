5083fabc1ab7e3e4d66acc5beb9ea10fe72d7572
Nicolaas Leenknegt <nicolaas.leenknegt.w1123@student.hogent.be>
2018-12-06 00:53:50 +0100

    aanpassing in /post/image router voor de image te kunnen uploaden

--- models/post.js
+++ models/post.js
@@ -8,7 +8,10 @@ let PostSchema = new mongoose.Schema({
     inhoud:String,
     afbeelding:String, // voorlopig als string
     user_id: mongoose.Schema.Types.ObjectId,
-    image_file_name:String
+    image_file_name:String,
+    session_name:String,
+    exercise_name:String,
+    page_name:String
 });
 
 mongoose.model('post', PostSchema);
